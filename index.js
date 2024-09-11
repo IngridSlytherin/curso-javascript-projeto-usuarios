8baf8386d83d1139d7114f93f2ee37d25fc58446
Ingrid <ingrid_patricia.cp@hotmail.com>
2024-09-11 11:15:44 -0300

    Primeiro commit

---
+++ index.js
@@ -0,0 +1,5 @@
+// Cria uma nova instância da classe UserController, passando dois parâmetros: 
+// o ID do formulário "form-user-create" e o ID da tabela "table-users".
+// Essa instância armazenada na variável `userController` é responsável por 
+// controlar a lógica de criação de novos usuários e a atualização da tabela de usuários na interface.
+let userController = new UserController("form-user-create", "form-user-update", "table-users");
